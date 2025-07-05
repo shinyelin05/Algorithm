6d8f6c35d592f216f0259b0d2781081a9a42aac7
shinyelin <77713669+shinyelin05@users.noreply.github.com>
2025-07-05 21:54:49 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> points(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> points[i].first >> points[i].second;
+    }
+
+    sort(points.begin(), points.end());
+
+    for (int i = 0; i < N; i++) {
+        cout << points[i].first << ' ' << points[i].second << '\n';
+    }
+
+    return 0;
+}
